204a93d537e09f0b0412fd5e49edb88c280c0f72
gshashi <shashi.g.s10@gmail.com>
2017-01-24 15:37:05 +0530

    added input output files reading

--- GFF2TAB.py
+++ GFF2TAB.py
@@ -1,4 +1,7 @@
-with open ("C:\Users\skumar20\Desktop\Bioinfo_dev\GFF_parser\samp.gff", "r") as gff_file:
-    data=gff_file.readlines()
-print data
-print "hello"
+with open("C:\Users\skumar20\Desktop\Bioinfo_dev\GFF_parser\samp1.gff", "r") as gff_file:
+    data = gff_file.readlines()
+gff_file.close()
+o_file = open("C:\Users\skumar20\Desktop\Bioinfo_dev\GFF_parser\samp2.gff","w")
+for line in data:
+    o_file.write(line)
+o_file.close()
